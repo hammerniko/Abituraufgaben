60ec4da1e350a10bd72cb4f2af455ebc7d4c4e53
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-01-22 14:53:33 +0100

    init

---
+++ Abituraufgaben/src/HP20022003/Person.java
@@ -0,0 +1,20 @@
+package HP20022003;
+
+public class Person {
+    
+    private String derVorname;
+
+    public Person(String einVorname) {
+         derVorname = einVorname;
+    }
+    
+
+    public void setDerVorname(String derVorname) {
+        this.derVorname = derVorname;
+    }
+    
+    
+    
+    
+
+}
